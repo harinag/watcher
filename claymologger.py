eaeda43323336490aee201d58cb5e8db0f5d0055
Max Home <max@huzm.ru>
2017-10-01 01:25:02 +0300

    Logger filesystem basic functional

--- claymologger.py
+++ claymologger.py
@@ -2,37 +2,79 @@
 
 import os
 import time
-import glob
-import logging
+import re
 from watchdog.observers import Observer
 import watchdog.events
+
 
 class MyHandler(watchdog.events.FileSystemEventHandler):
     """ Handler for Observer scheduler """
 
+    current_file = None
+    current_seek = 0
+
     def on_modified(self, event):
-        print(event, flush=True)
+        if event.is_directory:
+            return
+        fname = event.src_path
+        if fname != self.current_file:
+            if self.current_file is None:
+                self.on_created(event)
+        else:
+            self.parse_lines()
+
 
     def on_created(self, event):
-        print(event, flush=True)
+        if event.is_directory:
+            return
+        fname = event.src_path
+        if re.match('.*_log.txt$', fname):
+            self.__class__.current_file = fname
+            self.__class__.current_seek = 0
+            self.log_to_console('Swithced to new log file: ' + event.src_path)
+
+
+    def parse_lines(self):
+        with open(self.current_file, 'r') as f:
+            f.seek(self.current_seek)
+            cnt = 0
+            for line in f:
+                cnt += 1
+            self.log_to_console('Lines parsed: ' + str(cnt))
+            self.current_seek = f.tell()
+
+
+    def log_to_console(self, msg):
+        print(msg, flush=True)
+
+
+
+class ClayLogger:
+    """ Claymore log files parser """
+
+    def __init__(self):
+        self.current_file = None    # current log file name
+        self.current_seek = 0       # current position in current file
+
+
+    def run(self):
+        obs = Observer()
+        obs.schedule(MyHandler(), '.', recursive=False)
+        obs.start()
+        prop = '\|/-'
+        i = 0
+        try:
+            while True:
+                print('Logging ' + prop[i], end='\r', flush=True)
+                i += 1
+                if i == 4: i = 0
+                time.sleep(0.5)
+        except (KeyboardInterrupt, SystemExit):
+            print('\nCTRL+C detected! Stop logging.', flush=True)
+            obs.stop()
+        obs.join()
 
 
 # Start point
-
-obs = Observer()
-hndl = MyHandler()
-obs.schedule(hndl, '.', recursive=False)
-obs.start()
-prop = '\|/-'
-i = 0
-try:
-    while True:
-        print('Logging ' + prop[i], end='\r', flush=True)
-        i += 1
-        if i == 4: i = 0
-        time.sleep(0.5)
-except (KeyboardInterrupt, SystemExit):
-    print('\nCTRL+C detected! Stop logging.', flush=True)
-    obs.stop()
-obs.join()
-
+logger = ClayLogger()
+logger.run()
